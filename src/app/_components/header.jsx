5189ff6a7737831168c758dc0df97c25447f6c04
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2025-05-24 01:26:35 +0530

    commit

--- src/app/_components/header.jsx
+++ src/app/_components/header.jsx
@@ -4,19 +4,25 @@ import React from "react"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { useUser, UserButton } from "@clerk/nextjs"
+import Link from "next/link"
 
 function Header(){
     const { user, isSignedIn } = useUser();
     return (
-        <div className="p-5 flex justify-center items-center border shadow-sm">
-            <div>
-                {/*logo*/}
-                <span>Trackwise</span>
+        <div className="p-5 flex justify-between items-center border shadow-sm">
+            <div className="flex items-center">
+                <Image src={'/money-svgrepo-com.svg'} alt="logo" width={60}  height={60}/>
+                <span className="text-orange-400 font-bold text-xl">Trackwise</span>
             </div>
-            <div>
-                <button>button 1</button>
-                <button>button 2</button>
-            </div>
+            {isSignedIn ? (<UserButton />) : <div>
+                <Link href='/dashboard'>
+                    <Button>Dashboard</Button>
+                </Link>
+                <Link  href='/sign-in'> 
+                    <Button>Get Started</Button>
+                </Link>
+            </div> }
+            
         </div>
     )
 }
