72003da280ac0a8b22fd8c01f717dd2af8d76aa2
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2025-05-26 15:13:27 +0530

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Header from "./_components/header";
-import Hero from "./_components/hero";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
