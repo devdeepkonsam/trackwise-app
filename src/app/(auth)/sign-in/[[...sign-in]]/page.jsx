64c8c397af49cfbffbe8673365960c0f23cd1876
DEVDEEP SINGHA <devdeepkonsam1212@gmail.com>
2025-05-26 11:31:15 +0530

    commit

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -6,8 +6,8 @@ export default function Page() {
       <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
         <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
           <img
-            alt=""
-            src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt="logo"
+            src="/sign-up.jpg"
             className="absolute inset-0 h-full w-full object-cover opacity-80"
           />
         </section>
